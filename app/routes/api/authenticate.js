58843ee9ce1f2fe89520b928fb5bb9a183898696
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-01-16 20:25:30 +0100

    disallow admin to login to company account with his own name

--- app/routes/api/authenticate.js
+++ app/routes/api/authenticate.js
@@ -8,14 +8,14 @@ module.exports = function(app, path) {
 
     app.post(path, function(req, res) {
         // find the account
-        Account.getByNameOrMail([req.body.name, 'admin']).then((account) => {
-            console.log(account);
-            if (account[0].name === 'admin') {
+        Account.getByNameOrMail([req.body.name, 'admin', 'IBC-Admin']).then((account) => {
+            if (account[0].name === 'admin' || account[0].name === 'IBC-Admin') {
                 console.log('doesn\'t exist');
                 // username doesn't exist
                 res.status(403).json({ error: 'authentication failed. user doesn\'t exist or password is wrong' });
             } else if (account[0].password !== crypto.createHmac('sha512', account[0].salt).update(req.body.password).digest('hex')
-                        && account[1].password !== crypto.createHmac('sha512', account[1].salt).update(req.body.password).digest('hex')) {
+                        && account[1].password !== crypto.createHmac('sha512', account[1].salt).update(req.body.password).digest('hex')
+                        && account[2] && account[2].password !== crypto.createHmac('sha512', account[2].salt).update(req.body.password).digest('hex')) {
                 // password is incorrect
                 res.status(403).json({ error: 'authentication failed. user doesn\'t exist or password is wrong' });
             } else {
