8c910498870261aa867f8abe4d09b146cdce97c6
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-01-17 10:09:13 +0100

    fix security issue

--- app/routes/api/authenticate.js
+++ app/routes/api/authenticate.js
@@ -10,25 +10,23 @@ module.exports = function(app, path) {
         // find the account
         Account.getByNameOrMail([req.body.name, 'admin', 'IBC-Admin']).then((account) => {
             if (account[0].name === 'admin' || account[0].name === 'IBC-Admin') {
-                console.log('doesn\'t exist');
                 // username doesn't exist
                 res.status(403).json({ error: 'authentication failed. user doesn\'t exist or password is wrong' });
-            } else if (account[0].password !== crypto.createHmac('sha512', account[0].salt).update(req.body.password).digest('hex')
-                        && account[1].password !== crypto.createHmac('sha512', account[1].salt).update(req.body.password).digest('hex')
-                        && account[2] && account[2].password !== crypto.createHmac('sha512', account[2].salt).update(req.body.password).digest('hex')) {
-                // password is incorrect
-                res.status(403).json({ error: 'authentication failed. user doesn\'t exist or password is wrong' });
-            } else {
-
+            } else if (account[0].password === crypto.createHmac('sha512', account[0].salt).update(req.body.password).digest('hex')
+                        || account[1].password === crypto.createHmac('sha512', account[1].salt).update(req.body.password).digest('hex')
+                        || account[2] && account[2].password === crypto.createHmac('sha512', account[2].salt).update(req.body.password).digest('hex')) {
+                // password is either correct user or admin password
                 let payload = {
                     id: account[0].id,
                     name: account[0].name,
                     admin: account[0].admin
                 };
                 let token = jwt.sign(payload, app.get('secret'), { expiresIn: '8h'});
-
                 // return the token as JSON
                 res.status(200).json({ token: token });
+            } else {
+                // password is wrong
+                res.status(403).json({ error: 'authentication failed. user doesn\'t exist or password is wrong' });
             }
         })
     });
